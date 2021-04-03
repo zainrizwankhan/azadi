dab3fe7892ff9aca72eb8578652e83c4ff57de96
sajjadahmed677 <merledu677@gmail.com>
2021-04-03 15:29:46 +0500

    FPGA porting issues resolved

--- fpga/debug/debug.runs/impl_1/azadi_top_arty7.tcl
+++ fpga/debug/debug.runs/impl_1/azadi_top_arty7.tcl
@@ -65,6 +65,7 @@ start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
+  set_param xicom.use_bs_reader 1
   open_checkpoint azadi_top_arty7_routed.dcp
   set_property webtalk.parent_dir /home/merl/github_repos/azadi/fpga/debug/debug.cache/wt [current_project]
   set_property XPM_LIBRARIES XPM_CDC [current_project]
